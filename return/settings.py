441a8dcb684fd6bad231cd7e6803d5324b883273
RobsonMacedo <robson.s.macedo@gmail.com>
2021-04-01 23:22:09 -0300

    first commit

--- return/settings.py
+++ return/settings.py
@@ -21,7 +21,7 @@ django_heroku.settings(locals())
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -135,13 +135,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-''' STATICFILES_DIRS = [
-    '/code/pessoais/myvenv/return/core/static/css', '/code/pessoais/myvenv/return/core/static/js'
-] '''
+STATICFILES_DIRS = [BASE_DIR / "static", '/code/pessoais/myvenv/return/core/static/css', '/code/pessoais/myvenv/return/core/static/js']
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = [BASE_DIR / 'static']
 
